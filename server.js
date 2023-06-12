ebbc514d0299a0b3cb6b10cee48843c652192fdf
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-12 17:19:33 +0200

    working version

--- server.js
+++ server.js
@@ -2,18 +2,51 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
+/* Middleware */
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8080;
+
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+// GET method route
+app.get('/all', function (req, res) {
+    res.send(projectData);
+});
+
+// POST method route
+app.post('/addData', addData);
+
+function addData(req, res) {
+    const newData = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        content: req.body.content
+    };
+    projectData = newData;
+    res.send(projectData);
+    console.log('server is filled with data');
+    console.log(req.body.content);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,73 @@
 /* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+let apiKey = '&appid=48b6753b8d73fd35d09a779482d2500a';
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener for the element with the id: generate, with a callback function to execute when it is clicked.
+document.getElementById('generate').addEventListener('click', getWeatherDataFunction);
+
+// Callback function to execute when it is clicked.
+function getWeatherDataFunction(e) {
+    const zip = document.getElementById('zip').value;
+    getOpenWeatherData(baseURL, zip, apiKey);
+}
+
+// Write an async function in app.js that uses fetch() to make a GET request to the OpenWeatherMap API.
+const getOpenWeatherData = async (baseURL, zip, key) => {
+    const res = await fetch(baseURL + zip + key);
+    try {
+        const data = await res.json();
+        const temp = data.main.temp;
+        //console.log('temp: ' + temp)
+        postDataToServer(temp).then(updateUI);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+// Feelings Promise that makes a POST request to add the API data, as well as data entered by the user, to your app. 
+const postDataToServer = async (temp) => {
+    const feelings = document.getElementById('feelings').value;
+
+    const newData = {
+        temperature: temp,
+        date: newDate,
+        content: feelings,
+    };
+    //console.log('new Data: ' + newData.temperature)
+
+    const response = await fetch('/addData', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newData),
+    });
+    try {
+        const newData = await response.json();
+       // console.log(newData);
+        console.log('Data added successfully!');
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+
+}
+
+// Updates the UI dynamically
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('temp').innerHTML = allData.temperature;
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('content').innerHTML = allData.content;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
